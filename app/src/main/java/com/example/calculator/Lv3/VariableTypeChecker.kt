98d9a4c7c65e8bb65e297f9ca41a35aada626943
ars-yeon <zosel7301@gmail.com>
2023-07-19 20:21:08 +0900

    [FEAT]: ⚙️ isNumber 함수 delete

--- app/src/main/java/com/example/calculator/Lv3/VariableTypeChecker.kt
+++ app/src/main/java/com/example/calculator/Lv3/VariableTypeChecker.kt
@@ -1,16 +1,8 @@
 package com.example.calculator.Lv3
 import java.math.BigDecimal
 
+// 변수가 정수인지 아닌지 체크
 class VariableTypeChecker {
-    fun isNumber(input: String): Boolean {
-        return try {
-            BigDecimal(input)
-            true  // 숫자로 변환O
-        } catch (e: NumberFormatException) {
-            false  // 숫자로 변환X
-        }
-    }
-
     fun isInteger(num: BigDecimal): Boolean {
         return num.stripTrailingZeros().scale() <= 0
     }
