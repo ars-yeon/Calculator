6248ceb38843484e4157a8387c5c046f8612cf6d
ars-yeon <zosel7301@gmail.com>
2023-07-19 20:16:34 +0900

    [FEAT]: ⚙️ NumberChecker, OperatorChecker 기능 적용

--- app/src/main/java/com/example/calculator/Lv3/mainLv3.kt
+++ app/src/main/java/com/example/calculator/Lv3/mainLv3.kt
@@ -1,23 +1,24 @@
 package com.example.calculator.Lv3
-import java.math.BigDecimal
 import java.util.Scanner
 
 fun main() {
     val scanner = Scanner(System.`in`)
     val calc = Calculator()
-    val checker = VariableTypeChecker()
+    val numChecker = NumberChecker(scanner)  // num1, num2 입력값이 숫자인지 체크
+    val operatorChecker = OperatorChecker()  // operator 입력값이 '+', '-', '*', '/' 중에 있는지 없는지 체크
+    val typeChecker = VariableTypeChecker()  // 변수가 정수인지 아닌지 체크
 
     println("숫자를 입력하세요.")
-    var num1 = BigDecimal(scanner.next())
+    var num1 = numChecker.isNumber()
 
     println("연산자를 입력하세요. (+, -, *, /)")
-    var operator = scanner.next()[0]  // Scanner.next()로 첫번째 글자를 추출
+    var operator = operatorChecker.isOperator(scanner)
 
     println("숫자를 입력하세요.")
-    var num2 = BigDecimal(scanner.next())
+    var num2 = numChecker.isNumber()
 
     val result = calc.calculate(num1, operator, num2)
-    val finalResult = if (checker.isInteger(result)) result else result.toInt()
+    val finalResult = if (typeChecker.isInteger(result)) result else result.toInt()
 
     println("$num1 $operator $num2 = $finalResult")
 }
