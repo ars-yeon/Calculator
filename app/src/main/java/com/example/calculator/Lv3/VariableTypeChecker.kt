cd9d055f62e3c52b139ed5792bc3123bbf7d0af3
ars-yeon <zosel7301@gmail.com>
2023-07-19 17:35:51 +0900

    [FEAT]: ⚙️ 입력값이 숫자인지 아닌지 검사하는 기능 추가

--- app/src/main/java/com/example/calculator/Lv3/VariableTypeChecker.kt
+++ app/src/main/java/com/example/calculator/Lv3/VariableTypeChecker.kt
@@ -2,6 +2,15 @@ package com.example.calculator.Lv3
 import java.math.BigDecimal
 
 class VariableTypeChecker {
+    fun isNumber(input: String): Boolean {
+        return try {
+            BigDecimal(input)
+            true  // 숫자로 변환O
+        } catch (e: NumberFormatException) {
+            false  // 숫자로 변환X
+        }
+    }
+
     fun isInteger(num: BigDecimal): Boolean {
         return num.stripTrailingZeros().scale() <= 0
     }
